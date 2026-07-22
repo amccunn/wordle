896053042f7a14832a91681b43e036b756289ea2
amccunn <a.mccunn@lancaster.ac.uk>
2026-07-22 22:53:02 +0100

    Added wordle game logic

--- wordle-game.py
+++ wordle-game.py
@@ -8,12 +8,44 @@ with open("valid-words.csv", "r") as f:
 with open("word-bank.csv", "r") as fi:
     targetWordPossiblities = fi.read().splitlines()
 
-#ask for valid input
-guess = ""
-while guess not in validWords:
-
-    guess = input("What is your guess: ")
-
 targetWord = r.choice(targetWordPossiblities)
 
 print(targetWord)
+
+#wordle game logic
+guess = ""
+guesses = 0
+while guess != targetWord and guesses < 5:
+
+    #ask for valid input
+    guess = ""
+    while guess not in validWords:
+
+        guess = input("What is your guess: ")
+
+
+    wordList = list(guess)
+    targetWordList = list(targetWord)
+
+    tempTargetWordList = list(targetWord)
+    letterColours = ["grey"] * 5
+
+    for i, letter in enumerate(wordList):
+
+        if letter in tempTargetWordList:
+
+            letterColours[i] = "yellow"
+
+            if letter == tempTargetWordList[i]:
+
+                letterColours[i] = "green"
+
+            for j, targetLetter in enumerate(tempTargetWordList):
+
+                if letter == targetLetter:
+
+                    tempTargetWordList[j] = None
+
+    guesses += 1
+
+    print(letterColours)
